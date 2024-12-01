1b24c791bcb3af54edb595d59ce48d4860f77dde
Hargun Badhesha <162474291+hargunbadhesha@users.noreply.github.com>
2024-12-01 16:35:48 -0500

    Update TempoAnalysisController.java

--- src/main/java/interface_adapter/TempoAnalyser/TempoAnalysisController.java
+++ src/main/java/interface_adapter/TempoAnalyser/TempoAnalysisController.java
@@ -1,1 +1,28 @@
+package interface_adapter.TempoAnalyser;
+
+import use_case.TempoAnalyser.TempoAnalysisInputBoundary;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The controller class responsible for handling user actions related to tempo analysis.
+ */
+public class TempoAnalysisController {
+    private final TempoAnalysisInputBoundary inputBoundary;
+
+    public TempoAnalysisController(TempoAnalysisInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    public Map<String, Integer> handleAnalyzeTempoRequest(List<String> trackIds) {
+        try {
+            return inputBoundary.analyzeTempos(trackIds);
+        }
+        catch (IOException ioException) {
+            throw new RuntimeException("Error analyzing tempos: " + ioException.getMessage(), ioException);
+        }
+    }
+}
 
